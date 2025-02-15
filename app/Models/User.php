cdf2e5e3c62d73fac6cf8428e3e73891eeee7f74
Tuyen Nguyen Dinh <tuyennd@nal.vn>
2025-02-15 10:48:55 +0700

    Make a CheckOrganizationPermissionTrait and move then check organization permission function into the new trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,15 @@
 
 namespace App\Models;
 
+use App\Traits\CheckOrganizationPermissionTrait;
 use App\Traits\MediaTrait;
 use App\Traits\SetDateTimeConfigTrait;
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Auth;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Spatie\Permission\Traits\HasRoles;
@@ -25,6 +24,7 @@ class User extends Authenticatable
     use HasRoles;
     use SetDateTimeConfigTrait;
     use MediaTrait;
+    use CheckOrganizationPermissionTrait;
 
     /**
      * The attributes that are mass assignable.
@@ -156,7 +156,7 @@ class User extends Authenticatable
     /**
      * @return BelongsToMany
      */
-    public function organizationGroups()
+    public function organizationGroups(): BelongsToMany
     {
         return $this->belongsToMany(OrganizationUnit::class, UsersOrganizations::class);
     }
@@ -199,97 +199,5 @@ class User extends Authenticatable
     public function menus(): BelongsToMany
     {
         return $this->belongsToMany(Menu::class, UserMenu::class);
-    }
-
-    /**
-     * @param string $permission
-     * @param $organizationId
-     * @return bool
-     */
-    public function hasOrganizationPermission(string $permission, $organizationId): bool
-    {
-        // Check to see if the logged in organization is assigned to the user.
-        // This case occurs when the currently logged-in user has their permissions updated by the admin.
-        $requestOrganization = OrganizationUnit::query()->find(session('organization_id'));
-        $assignedOrganizations = $this->getAllAssignedOrganizationIds();
-        if (!in_array($requestOrganization->id, $assignedOrganizations)) {
-            return false;
-        }
-
-        // 1. Lấy danh sách các tổ chức cha và chính tổ chức cần kiểm tra quyền, bỏ qua global scope
-        $organizationAncestors = [];
-        if (is_array($organizationId)) {
-            foreach ($organizationId as $id) {
-                $orgs = OrganizationUnit::withoutGlobalScopes()
-                    ->ancestorsAndSelf($id)
-                    ->pluck('id')
-                    ->toArray();
-                $organizationAncestors = array_merge($organizationAncestors, $orgs);
-            }
-        } else {
-            $organizationAncestors = OrganizationUnit::withoutGlobalScopes()
-                ->ancestorsAndSelf($organizationId)
-                ->pluck('id')
-                ->toArray();
-        }
-        // 2. Truy vấn các vai trò của người dùng có liên kết với các tổ chức trong nhánh này, bỏ qua global scope
-        $userRoles = Auth::guard('web')->user()->roles()
-            ->withoutGlobalScopes()
-            ->whereHas('organizations', function ($query) use ($organizationAncestors) {
-                $query->whereIn('organization_id', $organizationAncestors);
-            })
-            ->get();
-        // 3. Kiểm tra quyền trên từng vai trò của người dùng
-        foreach ($userRoles as $role) {
-            if ($role->checkPermissionTo($permission)) {
-                return true; // Người dùng có quyền cần thiết trong một trong các vai trò
-            }
-        }
-
-        return false; // Người dùng không có quyền cần thiết
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getAllOrganizationTrees(): mixed
-    {
-        // Lấy danh sách ID của tất cả các tổ chức mà user này thuộc về
-        $organizationIds = $this->organizations()->withoutGlobalScopes()->pluck('organization_units.id');
-
-        // Lấy chính nó và tất cả con cháu của từng tổ chức trong danh sách
-        return OrganizationUnit::query()->withoutGlobalScopes()
-            ->whereIn('id', $organizationIds)
-            ->orWhereHas('ancestors', function ($query) use ($organizationIds) {
-                $query->whereIn('id', $organizationIds);
-            })
-            ->get()
-            ->toTree();
-    }
-
-    /**
-     * Get all assigned organization IDs.
-     *
-     * @return array
-     */
-    public function getAllAssignedOrganizationIds(): array
-    {
-        $ids = [];
-        $assignedOrganizations = $this->getAllOrganizationTrees();
-        foreach ($assignedOrganizations as $assignedOrganization) {
-            // pluck grand children ids
-            $grandIds = [];
-            foreach ($assignedOrganization->children as $child) {
-                $grandIds = array_merge($grandIds, $child?->children->pluck('id')->toArray() ?? []);
-            }
-
-            $ids = array_merge($ids, [
-                $assignedOrganization->id,
-                ...$assignedOrganization?->children->pluck('id')->toArray() ?? [],
-                ...$grandIds
-            ]);
-        }
-
-        return $ids;
     }
 }
